6f4ce286370f7968b78938a5359e2329d8b0a40a
Nick Salvatore <nmsalvatore@gmail.com>
2024-10-13 17:16:50 -0700

    feat: add url for new post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, post_list, post_detail, contact
+from .views import home, post_list, post_detail, new_post, contact
 
 urlpatterns = [
     path("", home, name="home"),
     path("posts/", post_list, name="post_list"),
+    path("posts/new/", new_post, name="new_post"),
     path("posts/<str:slug>/", post_detail, name="post_detail"),
     path("contact/", contact, name="contact"),
 ]
