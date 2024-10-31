9bc25b055173b98f57a13a0e806b5ce12e4560ac
Nick Salvatore <nmsalvatore@gmail.com>
2024-10-30 20:17:43 -0700

    feat: add Tag model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
     title = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -13,3 +28,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ["-created_on"]
