b61680c47e4aaa6ed573ff27fe46c9d341729ad3
Nick Salvatore <nmsalvatore@gmail.com>
2024-11-08 12:20:08 -0800

    feat: add filtering by tag

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,46 @@
 from collections import defaultdict
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.text import slugify
+from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from .models import Tag, Post
 from .forms import PostForm
 
 
 def home(request):
-    posts = Post.objects.all().order_by("-updated_on")[:5]
+    posts = Post.objects.all().order_by("-created_on")[:5]
     context = {"posts": posts}
     return render(request, "blog/home.html", context)
 
 
 def post_list(request):
-    posts = Post.objects.all().order_by("-updated_on")
-    posts_by_year = defaultdict(list)
+    tag_slug = request.GET.get("tag")
+    posts = Post.objects.all().order_by("-created_on")
 
+    current_tag = None
+    if tag_slug:
+        current_tag = get_object_or_404(Tag, slug=tag_slug)
+        posts = posts.filter(tags=current_tag)
+
+    posts_by_year = defaultdict(list)
     for post in posts:
         year = post.created_on.year
         posts_by_year[year].append(post)
 
     sorted_years = sorted(posts_by_year.items(), reverse=True)
-    tags = Tag.objects.all()
-    context = {"years_with_posts": sorted_years, "tags": tags}
+    tags = Tag.objects.annotate(
+            post_count=Count('posts')
+        ).order_by('-post_count', 'name')
+
+    context = {
+        "years_with_posts": sorted_years,
+        "tags": tags,
+        "current_tag": current_tag
+    }
+
+    if request.headers.get('HX-Request'):
+        return render(request, "blog/partials/post_list.html", context)
+
     return render(request, "blog/post_list.html", context)
 
 
@@ -84,11 +102,14 @@ def contact(request):
 
 def save_formatted_tags(request, post):
     formatted_tags = request.POST.get("formatted_tags")
+    post.tags.clear()
     if formatted_tags:
         tag_names = formatted_tags.split(",")
         for tag_name in tag_names:
-            tag, created = Tag.objects.get_or_create(
-                name=tag_name,
-                defaults={"slug": slugify(tag_name)}
-            )
-            post.tags.add(tag)
+            tag_name = tag_name.strip()
+            if tag_name:
+                tag, created = Tag.objects.get_or_create(
+                    name=tag_name,
+                    defaults={"slug": slugify(tag_name)}
+                )
+                post.tags.add(tag)
