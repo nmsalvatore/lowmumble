c8e7b73a1f2b5b18740ba23538cbac7ff12aa2c6
Nick Salvatore <nmsalvatore@gmail.com>
2024-11-29 13:20:48 -0800

    debug: print publish status in edit

--- blog/views.py
+++ blog/views.py
@@ -110,6 +110,8 @@ def edit_post(request, slug):
     submit_action = request.POST.get("submit_action")
     published = submit_action == "publish" or submit_action == "update"
 
+    print(published)
+
     if form.is_valid():
         try:
             post = form.save(commit=False)
